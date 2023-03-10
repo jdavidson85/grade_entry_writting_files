739fcccad64982a792b4ac422f1b1f725a034e62
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-03-10 14:06:43 -0600

    Add files via upload

--- grade_entry_program.py
+++ grade_entry_program.py
@@ -1,12 +1,22 @@
-n = int(input('How many students do you need to enter grades for? '))
-student_grades = []
-for i in range(n):
-    name = input(f'Enter the name of student {i+1}: ')
-    grade = input('Enter the student\'s final letter grade: ')
-    student_grades.append([name, grade])
-outfile = open('grades.txt', 'w')
-for line in student_grades:
-    lineout = '\'' + line[0] + '\', \'' + line[1] + '\'\n'
-    outfile.write(lineout)
-    
-outfile.close()
+def main():
+
+    line = []
+    student_grades = []
+    count = int(input("How many students do you need to "
+                      "enter grades for? "))
+    for student in range(0, count):
+        student_name = input("Enter students name: ")
+        grade = input("Enter students final letter grade: ")
+        line.append(student_name)
+        line.append(grade)
+        student_grades.append(line)
+        line = []
+    print(student_grades)
+
+    outfile = open("grades.txt", "w")
+    for line in student_grades:
+        lineout = "'" + line[0] + "','" + line[1] + "'/n"
+        outfile.writelines(lineout)
+    outfile.close()
+
+main()
